4f4b983d5f5fab1f318b3fd8df4d6411c4312a0b
admarcano <albertodiazmarcano@hotmail.com>
2025-11-20 14:20:27 +0100

    actualizacion del manifiest

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -2,5 +2,18 @@ import { withValidManifest } from "@coinbase/onchainkit/minikit";
 import { minikitConfig } from "../../../minikit.config";
 
 export async function GET() {
-  return Response.json(withValidManifest(minikitConfig));
+  const manifest = withValidManifest(minikitConfig);
+  
+  // Asegurar que baseBuilder esté incluido en el JSON
+  const response = {
+    ...manifest,
+    ...(minikitConfig.baseBuilder && { baseBuilder: minikitConfig.baseBuilder }),
+  };
+  
+  return Response.json(response, {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
 }
